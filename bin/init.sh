e810a5d039a342b76968fbc5f600e8c21d68cdf8
Mike Thorn <mike@TherapyCharts.com>
2014-08-06 11:43:15 -0400

    fixing init.sh to make it properly update all submodules

--- bin/init.sh
+++ bin/init.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env bash
 
-git clone git@github.com:twbs/bootstrap.git lib/bootstrap
-git clone git@github.com:Dev-Lucid/BootstrapConstructor.git lib/BootstrapConstructor
-git clone git@github.com:oyejorge/less.php.git lib/less.php
-git clone git@github.com:cowboy/jquery-hashchange.git lib/jquery-hashchange
-git clone git@github.com:Dev-Lucid/lucid-router.git lib/lucid-router
-
-git submodule foreach git pull origin master
+git submodule update --init --recursive
 
 echo "Done."
