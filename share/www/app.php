029bc8c18cdb393a8b3829d257595545b6c898bf
Mike Thorn <mike@TherapyCharts.com>
2014-12-27 15:05:21 -0500

    fixing config loading

--- share/www/app.php
+++ share/www/app.php
@@ -1,7 +1,8 @@
 <?php
 global $lucid;
-include(__DIR__.'/../etc/router.php');
-include(__DIR__.'/../etc/orm.php');
+include(__DIR__.'/../etc/router.php');  # load the router. You definitely need this.
+include(__DIR__.'/../etc/orm.php');     # load the ORM layer. You probably want this, or something equivalent.
+include(__DIR__.'/../etc/html.php');    # load the html library for components. You may want this.
 
 lucid::process();
 lucid::deinit();
